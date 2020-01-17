03def188138ed87c9dc950ac2c65652894aff302
leijin4wk <frank.lei@oyohotels.cn>
2020-01-17 10:38:45 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+let root = document.getElementById("root");
+let a=0;
+if(a){
+    console.log(11)
+}else{
+    console.log(22)
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const htmlPlugin = require('html-webpack-plugin');
+module.exports = {
+    //模块的入口文件
+    entry: {
+        path: "./src/main.js"
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.(js|jsx)$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new htmlPlugin({
+            minify: {
+                removeAttributeQuotes: true
+            },
+            hash: true,
+            template: './public/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        compress: true,
+        host: 'localhost',
+        port: 3000
+    }
+};
