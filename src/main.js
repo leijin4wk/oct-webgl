ea970e0d6f72bbd1216f29f4bad42839a2955cbd
leijin4wk <frank.lei@oyohotels.cn>
2020-03-04 13:03:26 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,27 +1,16 @@
-import {initShaderProgram} from './util/shaderUtil'
-
 import vertexShader from './shader/vertexShader.glsl'
 
 import fragmentShader from './shader/fragmentShader.glsl'
 
+const u = require('gl-util');
+
 const canvas = document.getElementById("canvas");
 
-const gl = canvas.getContext("webgl");
+const gl = u.context(canvas);
 
-const shaderProgram = initShaderProgram(gl, vertexShader, fragmentShader);
+const shaderProgram = u.program(gl, vertexShader, fragmentShader);
 
-gl.useProgram(shaderProgram);
+u.attribute(shaderProgram, 'position', [0,0, 1,0, 0,1]);
+u.uniform(shaderProgram, 'color', [1, .2, 0, 1.]);
 
-let positionLocation = gl.getAttribLocation(shaderProgram, "a_position");
-
-let buffer = gl.createBuffer();
-
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1.0, -1.0, 1.0, -1.0, -1.0,  1.0, -1.0,  1.0, 1.0, -1.0, 1.0,  1.0]), gl.STATIC_DRAW);
-
-gl.enableVertexAttribArray(positionLocation);
-
-gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-
-gl.drawArrays(gl.TRIANGLES, 0, 6);
+gl.drawArrays(gl.TRIANGLES, 0, 3);
--- src/shader/vertexShader.glsl
+++ src/shader/vertexShader.glsl
@@ -1,8 +1,7 @@
+precision mediump float;
 
-attribute vec2 a_position;
+attribute vec2 position;
 
 void main() {
-
-    gl_Position = vec4(a_position, 0, 1);
-
+    gl_Position = vec4(position * 2. - 1., 0, 1);
 }
